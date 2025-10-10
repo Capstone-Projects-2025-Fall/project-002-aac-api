fc3b5bfca146c0f70a0ff523f985345a5843b863
Kieran-Plenn <kieranplenn@gmail.com>
2025-10-10 12:47:20 -0400

    Added initial unit tests with Jest and Supertest

--- Initial_API/index.js
+++ Initial_API/index.js
@@ -3,10 +3,13 @@
  * Date: 10/03/2025
  * Description: This is a basic initialization of an API. Instructions were followed from this
  *              video: https://www.youtube.com/watch?v=-MTSQjw5DrM
+ * 
  * Note: Key 'bug' fix was to make sure the app.listen() function was not deleted. 
  * 
  * Run: Make sure you're in the Initial_API directory. Enter 'node .' into the terminal to start
  *      the API. Check http://localhost:8080 for success.
+ * 
+ * Last Edit (10/10/2025): Changed code to allow for unit testing with Jest
  */
 
 const express = require('express');
@@ -14,32 +17,34 @@ const app = express();
 const PORT = 8080;
 
 // Middleware to parse JSON
-app.use( express.json() )
-
-// Runs API on server
-app.listen(
-    PORT,
-    () => console.log(`it's alive on http://localhost:${PORT}`)
-)
+app.use(express.json());
 
 // GET
 app.get('/test', (req, res) => {
     res.status(200).send({
         name: 'Test1',
         status: 'test'
-    })
+    });
 });
 
-//POST
+// POST
 app.post('/test/:id', (req, res) => {
-    const{ id } = req.params
-    const{ info } = req.body
+    const { id } = req.params;
+    const { info } = req.body;
 
     if (!info) {
-        res.status(418).send({ message: 'No info!' })
+        return res.status(418).send({ message: 'No info!' });
     }
 
     res.send({
         name: `Test message with info: ${info} and ID: ${id}`,
     });
 });
+
+// Only start server if this file is run directly
+if (require.main === module) {
+    app.listen(PORT, () => console.log(`it's alive on http://localhost:${PORT}`));
+}
+
+// Export app for testing
+module.exports = app;
