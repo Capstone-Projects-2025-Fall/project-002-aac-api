7e6e5550be237e2c6734df33c087fc36cf4891d7
AceShadowKnight <andrew.blass@temple.edu>
2025-11-05 14:52:29 -0500

    comments to tests added

--- Initial_API/__tests__/api.test.js
+++ Initial_API/__tests__/api.test.js
@@ -4,7 +4,7 @@ const app = require('../index'); // Import your Express app
 
 describe('Basic API Tests', () => {
 
-
+  //checking if status code is correct when file is not given
   it('POST /upload without file should return 418', async () => {
     const res = await request(app)
       .post('/upload')
@@ -13,6 +13,7 @@ describe('Basic API Tests', () => {
     expect(res.body).toEqual({ message: 'No audio file uploaded' });
   });
 
+  //checking if staus code is correct when file cannot be transcribed
   it('POST /upload with empty file should return 300 and message', async () => {
     const filePath = path.join(__dirname, 'test-audio.mp3');
     const res = await request(app)
@@ -23,6 +24,7 @@ describe('Basic API Tests', () => {
     expect(res.body.message).toMatch(/Audio processing failed/i);
   });
 
+  //checking if status code is correct when file is given able to be transcribed
   it('POST /upload with TestRecording file should return 200 and have message', async () =>{
     const filePath = path.join(__dirname, 'TestRecording.wav');
     const res = await request(app)
@@ -31,12 +33,12 @@ describe('Basic API Tests', () => {
     expect(res.statusCode).toBe(200);
   });
 
+  //checking if speech to text is accurate to the file
     it('POST /upload with TestRecording file should return correct transcript', async () =>{
     const filePath = path.join(__dirname, 'TestRecording.wav');
     const res = await request(app)
       .post('/upload')
       .attach('audioFile', filePath);
-    expect(res.statusCode).toBe(200);
     expect(res.body.transcription).toMatch(/ice cream trucks are really cool/i);
   });
 });
